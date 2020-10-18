f6315b36de09dca438526daf72a9660d55a7ad4f
willzittlau <67138318+willzittlau@users.noreply.github.com>
2020-10-17 19:49:41 -0700

    Some more basic styling

--- components/global/Resume.vue
+++ components/global/Resume.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="text-center">
     <a
-      class="mx-auto dark:text-white text-lightgrey text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-extrabold"
+      class="w-auto inline-block mx-auto dark:text-white text-lightgrey text-5xl sm:text-6xl md:text-7xl lg:text-8xl items-center font-extrabold border-b-8 border-indigo-600"
       href="download/Will Zittlau - Resume.pdf"
       target="_blank">
       Hire me
     </a>
-    <p class="dark:text-gray-400 font-bold pb-8">
+    <p class="dark:text-gray-400 font-bold py-8">
       Click above for a link to my resume
     </p>
   </div>
