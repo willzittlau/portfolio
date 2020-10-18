1757d241df50a51928fd5882e8d1c40de9fc68bc
willzittlau <67138318+willzittlau@users.noreply.github.com>
2020-10-17 19:42:14 -0700

    open resume in new tab, testing line function

--- components/global/Resume.vue
+++ components/global/Resume.vue
@@ -3,7 +3,11 @@
     <a
       class="mx-auto dark:text-white text-lightgrey text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-extrabold"
       href="download/Will Zittlau - Resume.pdf"
-      >Hire me</a>
-    <p class="dark:text-gray-400 font-bold pb-8">Click above for a link to my resume</p>
+      target="_blank">
+      Hire me
+    </a>
+    <p class="dark:text-gray-400 font-bold pb-8">
+      Click above for a link to my resume
+    </p>
   </div>
 </template>
