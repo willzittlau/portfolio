0098183c859d64405c64fa6de79c8fcb04811c9a
willz <willzittlau@outlook.com>
2021-01-26 22:39:08 -0800

    fix config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 export default {
   // Target (https://go.nuxtjs.dev/config-target)
-  mode: 'universal',
+  ssr: false,
   target: "static",
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
