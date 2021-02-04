6c36faaad6ea8d26e688e9684bc7f140e7581706
willz <willzittlau@outlook.com>
2021-02-03 16:40:52 -0800

    add technologies x2

--- components/global/Technologies.vue
+++ components/global/Technologies.vue
@@ -5,6 +5,7 @@
     </h1>
     <!--Technology SVGs are from From svgporn.com -->
     <div class="grid grid-cols-4 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-6 gap-6 md:gap-12 place-items-center pt-8 pb-8">
+      <div><img src="~assets/img/svg/vs-code.svg" alt="VS Code" /></div>
       <div><img src="~assets/img/svg/python.svg" alt="Python" /></div>
       <div><img src="~assets/img/svg/javascript.svg" alt="Javascript" /></div>
       <div><img src="~assets/img/svg/html-5.svg" alt="HTML" /></div>
@@ -12,17 +13,18 @@
       <div><img src="~assets/img/svg/flutter.svg" alt="Flutter" /></div>
       <div><img src="~assets/img/svg/flask.svg" alt="Flask" /></div>
       <div><img src="~assets/img/svg/drupal.svg" alt="Drupal" /></div>
+      <div><img src="~assets/img/svg/react.svg" alt="React" /></div>
       <div><img src="~assets/img/svg/nuxt.svg" alt="Nuxt" /></div>
       <div><img src="~assets/img/svg/bootstrap.svg" alt="Bootstrap" /></div>
       <div><img src="~assets/img/svg/sass.svg" alt="Sass" /></div>
       <div><img src="~assets/img/svg/shopify.svg" alt="Shopify" /></div>
+      <div><img src="~assets/img/svg/twilio.svg" alt="Twilio" /></div>
       <div><img src="~assets/img/svg/git.svg" alt="Git" /></div>
       <div><img src="~assets/img/svg/jira.svg" alt="Jira" /></div>
       <div><img src="~assets/img/svg/netlify.svg" alt="Netlify" /></div>
       <div><img src="~assets/img/svg/firebase.svg" alt="Firebase" /></div>
       <div><img src="~assets/img/svg/heroku.svg" alt="Heroku" /></div>
       <div><img src="~assets/img/svg/postman.svg" alt="Postman" /></div>
-      <div><img src="~assets/img/svg/vs-code.svg" alt="VS Code" /></div>
     </div>
   </div>
 </template>
